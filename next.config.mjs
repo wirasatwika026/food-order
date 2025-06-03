ae7102280bac993a08a7cd3680634995e3d35cb8
Yordy Wira <kevinmacmasel@gmail.com>
2025-06-03 21:16:52 +0700

    add menu card component and fetch menu data from Supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "supabase.theawe.web.id", // Replace with your image host
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   output: "standalone",
 };
 
